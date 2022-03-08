59edfc3d65bdcbe26b77b640553f8d35fae4f800
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-03-07 19:04:28 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     $("#curiosidad").hover(function () {
         $("#imgHome1").css("display", "block");
+        // $("#imgHome1").css("mostrar", "desaparecer 0.8s ease");
         $("#imgHome2").css("display", "block");
         $("#imgHome3").css("display", "block");
         $("#imgHome4").css("display", "block");
     }, function () {
+        // $("#imgHome1").css("animation", "desaparecer 0.8s ease");
         $("#imgHome1").css("display", "none");
         $("#imgHome2").css("display", "none");
         $("#imgHome3").css("display", "none");
